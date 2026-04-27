44ef1fdcf2559592328de97a36820d13696e9c7e
Mizuki Octa <thecreativemanoff1@gmail.com>
2026-04-27 07:45:49 +0700

    emergency fallback initiated Signed off by Octauuzy

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,7 @@
 <?php
 
-use App\Http\Controllers\BukuController;
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    if (auth()->check()) {
-        return redirect()->route('buku.index');
-    }
-
-    return redirect()->route('login');
+    return view('welcome');
 });
-
-Route::get('/dashboard', function () {
-    return redirect()->route('buku.index');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::resource('buku', BukuController::class);
-
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
